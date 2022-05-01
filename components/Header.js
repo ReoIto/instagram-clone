15f470c848d6578067af5f2ffbc7528692befc75
Reo Ito <leo.0210.ito@gmail.com>
2022-05-01 18:08:25 +0900

    make Header being sticky to the top

--- components/Header.js
+++ components/Header.js
@@ -4,48 +4,50 @@ import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
-          alt="instagram text logo"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
-          alt="instagram logo"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <SearchIcon className="h-5 text-gray-500" />
+    <div className="shadow-md border-b sticky top-0 bg-white">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
+            alt="instagram text logo"
+            layout="fill"
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://support.apple.com/content/dam/edam/applecare/images/en_US/psp_content/content-block-md-memoji_2x.png"
-          alt="user image"
-          className="h-10 rounded-full cursor-pointer"
-        />
-        {/* <Image
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
+            alt="instagram logo"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://support.apple.com/content/dam/edam/applecare/images/en_US/psp_content/content-block-md-memoji_2x.png"
+            alt="user image"
+            className="h-10 rounded-full cursor-pointer"
+          />
+          {/* <Image
           src="https://support.apple.com/content/dam/edam/applecare/images/en_US/psp_content/content-block-md-memoji_2x.png"
           alt="user image"
           height={40}
           width={40}
           className="object-contain rounded-full"
         /> */}
+        </div>
       </div>
     </div>
   );
