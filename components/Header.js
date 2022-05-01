c93656690688e952c442c091919aef96d0bade7d
Reo Ito <leo.0210.ito@gmail.com>
2022-05-01 18:02:22 +0900

    add some icons on Header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
@@ -20,7 +21,6 @@ export default function Header() {
           className="object-contain"
         />
       </div>
-
       <div className="relative mt-1">
         <div className="absolute top-2 left-2">
           <SearchIcon className="h-5 text-gray-500" />
@@ -31,8 +31,22 @@ export default function Header() {
           className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
         />
       </div>
-
-      <h1>Right side</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          src="https://support.apple.com/content/dam/edam/applecare/images/en_US/psp_content/content-block-md-memoji_2x.png"
+          alt="user image"
+          className="h-10 rounded-full cursor-pointer"
+        />
+        {/* <Image
+          src="https://support.apple.com/content/dam/edam/applecare/images/en_US/psp_content/content-block-md-memoji_2x.png"
+          alt="user image"
+          height={40}
+          width={40}
+          className="object-contain rounded-full"
+        /> */}
+      </div>
     </div>
   );
 }
