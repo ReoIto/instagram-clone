6446f85e67aaeea459d1690706f9a5c8d62081a4
Reo Ito <leo.0210.ito@gmail.com>
2022-05-19 23:13:54 +0900

    modify MiniProfile and Story components to include the session

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,15 +1,23 @@
+import { useSession, signOut } from "next-auth/react";
+
 export default function MiniProfile() {
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 rounded-full border p-[2px] bg-white"
-        src="https://support.apple.com/content/dam/edam/applecare/images/en_US/psp_content/content-block-md-memoji_2x.png"
+        src={session?.user.image}
+        alt={session?.user.name}
       />
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">userName</h2>
+        <h2 className="font-bold">{session?.user.name}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram clone</h3>
       </div>
-      <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+        Sign out
+      </button>
     </div>
   );
 }
--- components/Stories.js
+++ components/Stories.js
@@ -2,9 +2,12 @@ import minifaker from "minifaker";
 import "minifaker/locales/en";
 import { useEffect, useState } from "react";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 
 export default function Stories() {
   const [storyUsers, setStoryUsers] = useState([]);
+  const { data: session } = useSession();
+
   useEffect(() => {
     const storyUsers = minifaker.array(20, (i) => ({
       username: minifaker.username({ local: "en" }).toLowerCase(),
@@ -16,6 +19,13 @@ export default function Stories() {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border border-gray-200 overflow-x-scroll rounded-sm scrollbar-none">
+      {session && (
+        <Story
+          img={session.user.image}
+          username={session.user.name}
+          isUser="true"
+        />
+      )}
       {storyUsers.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
