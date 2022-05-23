443394548d6a95db4468ae65dc9bf7820d9b7548
Reo Ito <leo.0210.ito@gmail.com>
2022-05-23 21:01:20 +0900

    fix UploadModal component

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -5,20 +5,22 @@ import Modal from "react-modal";
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
 
+  function onClickClose() {
+    setOpen(false);
+  }
+
   return (
     <div>
       <div>UploadModal</div>
-      {open && (
-        <Modal
-          isOpen={open}
-          onRequestClose={() => setOpen(false)}
-          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md outline-none"
-        >
-          <div className="flex flex-col justify-center items-center h-[100%]">
-            <h1>Modal</h1>
-          </div>
-        </Modal>
-      )}
+      <Modal
+        isOpen={open}
+        onRequestClose={onClickClose}
+        className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md outline-none"
+      >
+        <div className="flex flex-col justify-center items-center h-[100%]">
+          <h1>Modal</h1>
+        </div>
+      </Modal>
     </div>
   );
 }
